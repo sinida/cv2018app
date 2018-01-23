eaaa2a8d4ce61d2b96aa6413a70a6467d7325282
wesun-PC\wesun <foryou.charles@gmail.com>
2018-01-23 08:26:51 +0800

    Commit ok version ( It can work)

--- cv2018app/cv2018app/main.cpp
+++ cv2018app/cv2018app/main.cpp
@@ -42,22 +42,11 @@
 #include <stdio.h>
 
 using namespace std;
-using namespace cv;
-
-/** Function Headers */
-void detectAndDisplay(Mat frame);
-
-/** Global variables */
-//String face_cascade_name = "model/opencv/haarcascade_frontalface_alt.xml";
-//String eyes_cascade_name = "model/opencv/haarcascade_eye_tree_eyeglasses.xml";
-//CascadeClassifier face_cascade;
-//CascadeClassifier eyes_cascade;
-string window_name = "Capture - Face detection";
-//RNG rng(12345);
+//using namespace cv;
 
 int facedetect(cv::Mat in_img, std::vector<cv::Rect> &faceRect){
 
-	String face_cascade_name = "model/opencv/haarcascade_frontalface_alt.xml";
+	cv::String face_cascade_name = "model/opencv/haarcascade_frontalface_alt.xml";
 	cv::CascadeClassifier face_cascade;
 	if (!face_cascade.load(face_cascade_name)){ printf("--(!)Error loading - [Face model]\n"); return -1; };
 
@@ -71,7 +60,7 @@ int facedetect(cv::Mat in_img, std::vector<cv::Rect> &faceRect){
 	cv::equalizeHist(frame_gray, frame_gray);
 
 	//-- Detect faces
-	face_cascade.detectMultiScale(frame_gray, faceRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+	face_cascade.detectMultiScale(frame_gray, faceRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
 
 	return 0;
 }
@@ -87,7 +76,7 @@ int face_draw(std::vector<cv::Rect> faceRect, cv::Mat &out_img){
 }
 
 int eyedetect(cv::Mat in_img, std::vector<cv::Rect> &eyeRect){
-	String eyes_cascade_name = "model/opencv/haarcascade_eye_tree_eyeglasses.xml";
+	cv::String eyes_cascade_name = "model/opencv/haarcascade_eye_tree_eyeglasses.xml";
 	cv::CascadeClassifier eyes_cascade;
 	if (!eyes_cascade.load(eyes_cascade_name)){ printf("--(!)Error loading-[eye model]\n"); return -1; };
 	//-- In each face, detect eyes
@@ -110,7 +99,7 @@ int eye_draw(std::vector<cv::Rect> eyeRect, cv::Mat &out_img){
 int main(int argc, const char** argv)
 {
 
-	Mat frame;
+	cv::Mat frame;
 	//-- 2. Read the video stream
 
 	frame = cv::imread("Imagedata/facetest.jpg", 1);
@@ -135,37 +124,3 @@ int main(int argc, const char** argv)
 
 	return 0;
 }
-//
-///** @function detectAndDisplay */
-//void detectAndDisplay(Mat frame)
-//{
-//	std::vector<Rect> faces;
-//	Mat frame_gray;
-//
-//	cvtColor(frame, frame_gray, CV_BGR2GRAY);
-//	equalizeHist(frame_gray, frame_gray);
-//
-//	//-- Detect faces
-//	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
-//
-//	for (size_t i = 0; i < faces.size(); i++)
-//	{
-//		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
-//		ellipse(frame, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-//
-//		Mat faceROI = frame_gray(faces[i]);
-//		std::vector<Rect> eyes;
-//
-//		//-- In each face, detect eyes
-//		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
-//
-//		for (size_t j = 0; j < eyes.size(); j++)
-//		{
-//			Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
-//			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-//			circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
-//		}
-//	}
-//	//-- Show what you got
-//	imshow(window_name, frame);
-//}
